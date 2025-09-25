98f20778789afc9ff477b8f0ad657ae3891d5077
Peter <xervipeter@gmail.com>
2025-09-25 16:24:57 +0300

    update

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your code here!
+// Write your code here!// Remove the <main id="main"> element
+const main = document.getElementById("main");
+if (main) {
+  main.remove();
+}
+
+// Create a new <h1> element
+const newHeader = document.createElement("h1");
+
+// Set its id to 'victory'
+newHeader.id = "victory";
+
+// Set its inner text to your name and message
+newHeader.textContent = "Peter is the champion";
+
+// Append it to the body
+document.body.appendChild(newHeader);
